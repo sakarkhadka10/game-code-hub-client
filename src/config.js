d84730f246f4118dac958ec341311a6a48ee57a6
Sakar Khadka <khadka.sakar10@gmail.com>
2025-03-09 12:07:28 +0545

    mongo fixed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Name Of The Website
-export const PROJECT_NAME = "Game Code Hub";
+export const PROJECT_NAME = "Game Code Code";
 
 // Notifications For Website
 export const NOTIFICATION_DESKTOP = {
